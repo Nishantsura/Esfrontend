89a7f1e67f0d0f7acbdf7793804cc3d14d2f19ac
srikar <srikar9reddy@gmail.com>
2024-12-21 19:57:37 +0530

    cors fixeeddd

--- server.js
+++ server.js
@@ -25,10 +25,17 @@ const corsOptions = {
     const allowedOrigins = [
       process.env.FRONTEND_URL,
       'http://localhost:3001',
-      'http://localhost:3000'
+      'http://localhost:3000',
+      'https://*.vercel.app'
     ].filter(Boolean);
     
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+    if (!origin || allowedOrigins.some(allowedOrigin => {
+      if (allowedOrigin.includes('*')) {
+        const pattern = new RegExp('^' + allowedOrigin.replace('*', '.*') + '$');
+        return pattern.test(origin);
+      }
+      return allowedOrigin === origin;
+    })) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
